7924cc6a5c20ed9328a87d79093451054147c44d
Alex <alex@gmail.com>
2025-04-10 16:01:03 +0300

    made game slice with state i think i will need

--- src/features/PickMark/ButtonsMark/ButtonMarkWrapper.tsx
+++ src/features/PickMark/ButtonsMark/ButtonMarkWrapper.tsx
@@ -4,15 +4,18 @@ type PropsType = {
   inlineStyle?: { style: React.CSSProperties };
   classStyle?: string;
   children: React.ReactNode;
+  handleClick: () => void;
 };
 
 const ButtonMarkWrapper: FC<PropsType> = ({
   children,
   inlineStyle,
   classStyle,
+  handleClick,
 }) => {
   return (
     <button
+      onClick={handleClick}
       type="button"
       className={`w-full min-h-full rounded-[15px] flex justify-center items-center cursor-pointer transition-all duration-300 ${classStyle}`}
       {...inlineStyle}
