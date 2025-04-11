77b58ebea7704d83020d0178be16864560604727
Alex <alex@gmail.com>
2025-04-11 18:01:33 +0300

    start popup

--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -69,6 +69,7 @@ export const updateStoragePending = (gameState: GameStateType) => {
   const updated: GameStateType = {
     ...gameState,
     isPending: false,
+    isSuccess: false,
   };
 
   saveStorageGame(updated);
