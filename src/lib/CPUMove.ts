f4d4043da0ef36eae19f9411b77342c2ee62d84e
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-04-11 15:28:29 +0300

    start testing method with string comparison

--- src/lib/CPUMove.ts
+++ src/lib/CPUMove.ts
@@ -1,5 +1,5 @@
-import { GameStateType } from "../features/Game/gameSlice";
-import { endGame } from "./../config/endGame";
+import { chanceWin } from "../config/endGame";
+import { EndGameType, GameStateType } from "../features/Game/gameSlice";
 
 // adding 1 to max - min max is included in range
 export const makeRandom = (min: number, max: number) =>
@@ -32,13 +32,31 @@ export const makeMoveCPU = (gameState: GameStateType) => {
     : filtered[makeRandom(0, filtered.length - 1)];
 };
 
-export type EndGameType = (typeof endGame)[number];
-
 export const establishEndGame = (gameState: GameStateType): EndGameType => {
   const { gridGame, user, CPU } = gameState;
 
-  const userMark = user.mark;
-  const CPUmark = CPU.mark;
+  const vals = gridGame.map((el) => (el.val ? el : null));
+  let winner: EndGameType = null;
 
-  const indexVals = gridGame.map((el, i) => (el.val ? i : null));
+  if (vals.length < 6) return winner;
+
+  let indexUser: string = "";
+  let indexCPU: string = "";
+
+  let j = 0;
+  while (j < vals.length) {
+    const curr = vals[j];
+    if (curr?.val === user.mark) indexUser += j + ",";
+    else if (curr?.val === CPU.mark) indexCPU += j + ",";
+
+    j++;
+  }
+
+  if (chanceWin.some((str) => indexUser.includes(str))) winner = "user";
+  else if (chanceWin.some((str) => indexCPU.includes(str))) winner = "CPU";
+
+  if (winner === null && vals.every((el) => typeof el?.val === "string"))
+    winner = "tie";
+
+  return winner;
 };
