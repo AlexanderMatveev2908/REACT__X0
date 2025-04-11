a37432e58642e12017a3ad1439b9f97ca07db2f9
Alex <alex@gmail.com>
2025-04-11 16:44:25 +0300

    created my onw X and 0, still have some problems with plugin of svg

--- src/components/MiniMarks/MiniX.tsx
+++ src/components/MiniMarks/MiniX.tsx
@@ -2,16 +2,21 @@ import { FC, useState } from "react";
 import { v4 } from "uuid";
 import { setStyle } from "../../lib/styleSetter";
 
-const MiniX: FC = () => {
+type PropsType = {
+  classesCustom?: string;
+  inlineStyle?: { style: React.CSSProperties };
+};
+
+const MiniX: FC<PropsType> = ({ classesCustom, inlineStyle }) => {
   const [idsX] = useState<string[]>(Array.from({ length: 2 }, () => v4()));
 
   return idsX.map((id, i) => (
     <div
       key={id}
-      className={`min-h-[37px] min-w-[10px] CENTER_A_DIV😎 rounded-[3px] ${
-        !i ? "rotate-45" : "-rotate-45"
-      }`}
-      {...setStyle({ backgroundColor: "var(--white__2)" })}
+      className={`${
+        classesCustom ?? "min-h-[37px] min-w-[10px]"
+      } CENTER_A_DIV😎 rounded-[3px] ${!i ? "rotate-45" : "-rotate-45"}`}
+      {...(inlineStyle ?? setStyle({ backgroundColor: "var(--white__2)" }))}
     ></div>
   ));
 };
